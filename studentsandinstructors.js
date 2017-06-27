8d5aadcbcba29213317310036ce06a359a996c19
Jason <jason.w.switzer@gmail.com>
2017-06-26 23:14:18 -0700

    set up objects

---
+++ studentsandinstructors.js
@@ -0,0 +1,7 @@
+var students = [
+     {first_name:  'Michael', last_name : 'Jordan'},
+     {first_name : 'John', last_name : 'Rosales'},
+     {first_name : 'Mark', last_name : 'Guillen'},
+     {first_name : 'KB', last_name : 'Tonel'}
+]
+console.log(students[0].first_name + " " + students[0].last_name);
